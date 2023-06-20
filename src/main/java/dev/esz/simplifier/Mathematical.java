3d944cf052d83eb0fcb37061b7e6a563c646101f
EduardoStarZ <pathver@gmail.com>
2023-06-19 21:46:21 -0300

    deleted the random subclass from Mathematical.java

--- src/main/java/dev/esz/simplifier/Mathematical.java
+++ src/main/java/dev/esz/simplifier/Mathematical.java
@@ -52,7 +52,7 @@ public class Mathematical {
          * <p>
          * A java method that privately calculates the factorial of the provided number.
          * </p>
-         * 
+         *
          * @since 1.0.0
          */
         private void factorialOperation() {
@@ -66,13 +66,11 @@ public class Mathematical {
         }
 
         /**
-         * 
          * <p>A static method that receives a number and calculates it's factorial.
          * </p>
-         * 
+         *
          * @param number the number to have the factorial calculated.
          * @return an integer containing the factorial of the provided number.
-         * 
          * @since 1.0.0
          */
         public static int factore(int number) {
@@ -84,77 +82,6 @@ public class Mathematical {
             start.factorialOperation();
 
             return factorial;
-        }
-    }
-    /**
-     * @author EduardoStarZ
-     *
-     * @since 1.0.0
-     * */
-    public static class random<T>{
-        T obj;
-
-        public random(T obj) {
-            this.obj = obj;
-        }
-        
-        public void showType() {
-            System.out.println("Type is "+ obj.getClass().getName());
-        }
-
-        private String getType() {
-            return obj.getClass().getName();
-        }
-
-        public void generate() {
-            random<T> randomObject = new random<>(obj);
-
-            Random myRandom = new Random();
-            String type = randomObject.getType();
-
-            switch (type) {
-                case "java.lang.String":
-                    ;
-                    break;
-
-                case "java.lang.Integer":
-                    break;
-
-                case "java.lang.Float":
-                    ;
-                    break;
-
-                case "java.lang.Double":
-                    ;
-                    break;
-
-                case "java.lang.Short":
-                    ;
-                    break;
-
-                case "java.lang.Long":
-                    ;
-                    break;
-
-                case "java.lang.Byte":
-                    ;
-                    break;
-
-                case "java.lang.Boolean":
-                    ;
-                    break;
-            }
-
-            //java.lang.String
-            //java.lang.Integer
-            //java.lang.Character
-            //java.lang.Double
-            //java.lang.Long
-            //java.lang.Short
-            //java.lang.Float
-            //java.lang.Boolean
-            //java.lang.Byte
-
         }
     }
 }
